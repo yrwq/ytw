fa5670285c5321d507b45cbb1178d1bff73884a8
yrwq <yrwqid@gmail.com>
2020-11-25 09:34:28 +0100

    update

--- search.py
+++ search.py
@@ -31,7 +31,7 @@ try:
             help()
 
         elif currentArgument in ("-t", "--title"):
-            search = SearchVideos(sys.argv[2], offset = 1, mode = "json", max_results = 5)
+            search = SearchVideos(sys.argv[2], offset = 1, mode = "json", max_results = 10)
             print(search.result())
         else:
             help()
